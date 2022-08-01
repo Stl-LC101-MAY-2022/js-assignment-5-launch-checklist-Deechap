c102f4ab9225ab2d6aa0ef59e89bb87395f09b7e
Dee Chap <deniseechappell@gmail.com>
2022-08-01 13:17:13 -0500

    TEST

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-const { formSubmission } = require("./scriptHelper");
+const { formSubmission, addDestinationInfo, pickPlanet } = require("./scriptHelper");
 window.addEventListener("load", function() {
     let listedPlanets;
     let listedPlanetsResponse = myFetch();
@@ -9,9 +9,10 @@ window.addEventListener("load", function() {
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
-    //    Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destina\tion.    // 
+    //    Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.    // 
+pickPlanet();
 
-    let selectedPlanet=pickPlanet(listedPlanets) 
+let selectedPlanet=pickPlanet(listedPlanets) 
 selectedPlanet.then(function (result){
     selectedPlanet = result;
     console.log(selectedPlanet);
@@ -19,18 +20,22 @@ selectedPlanet.then(function (result){
     console.log(selectedPlanet);
 })
 // call function addDestinationInfo and pass in appropriate parameters (this will be end of fetch stuff)
+addDestinationInfo(){
+
+}
 })
  let faultyItems = document.getElementById('faultyItems');
 //  set visibility
     let form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function(event) {  
         event.preventDefault();
     let pilotName = document.querySelector("input[name=pilotName]");
     let copilotName = document.querySelector("input[name=copilotName]");
     let fuelLevel = document.querySelector("input[name=fuelLevel]");
     let cargoMass = document.querySelector("input[name=cargoMass]");
-formSubmission();
     
+formSubmission();
+   
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse;
@@ -39,6 +44,5 @@ formSubmission();
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
-   
+    }
+    
